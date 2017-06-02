ac48f43913b740589c28c3461532755a9f77d0b7
FloreU <lingxiucehua@163.com>
2017-06-02 11:31:40 +0800

    init2

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,9 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
+## The first function, makeCacheMatrix creates a special "vector", which is really a list containing a function to
+# set the value of the matrix
+# get the value of the matrix
+# set the value of the inverse
+# get the value of the inverse
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
     set <- function(y) {
@@ -18,7 +19,12 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## The following function calculates the inverse 
+## of the special "matrix" created with the makeCacheMatrix function. 
+## However, it first checks to see if the inverse has already been calculated. 
+## If so, it gets the inverse from the cache and skips the computation. 
+## Otherwise, it calculates the inverse of the data and sets the value of 
+## the inverse in the cache via the setsolve function.
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
@@ -32,3 +38,33 @@ cacheSolve <- function(x, ...) {
     x$setsolve(m)
     m
 }
+
+## Test case
+
+## Get a random matrix
+## set.seed(199)
+## m <- matrix(rnorm(1000000), 1000,1000)
+## solve(m)
+
+## Use makeCacheMatrix() and cacheSolve(), and get the running time
+## mymatrix = makeCacheMatrix()
+## mymatrix$set(m)
+## system.time({
+##     cacheSolve(mymatrix)
+## })
+## system.time({
+##     cacheSolve(mymatrix)
+## })
+## system.time({
+##     cacheSolve(mymatrix)
+## })
+
+## Running time results
+## user  system elapsed 
+## 1.946   0.004   1.954 
+## getting cached data
+## user  system elapsed 
+## 0.002   0.001   0.002 
+## getting cached data
+## user  system elapsed 
+## 0.002   0.000   0.002 
